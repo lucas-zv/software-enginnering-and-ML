c2549bb26cedf41f61579e29eb89a84d1e13ae49
lucas <lucaszvunca033@gmail.com>
2026-07-12 16:24:36 +0100

    type casting.cpp
    nothing to special just plain old type casting

---
+++ type casting.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+    using std::cout;
+    
+    int correct = 5;
+    int total = 12;
+    int percentage = correct /(double)total * 100;
+
+    cout << percentage << "%" << "\n";
+
+    char x = 100;
+    cout << x;
+
+    return 0;
+}
+
