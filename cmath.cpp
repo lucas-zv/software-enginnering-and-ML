e27e46f513571aafd5e7ae9668c44c68ab6f913e
lucas <lucaszvunca033@gmail.com>
2026-07-12 22:45:01 +0100

    cmath.cpp

---
+++ cmath.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+    using std::cout;
+
+    double x = 3.14;
+    double y = 2.94;
+    double z;
+
+    //z = std::max(x, y);
+    //z = std::min(x, y);
+    //z = pow(x, y);
+    //z = sqrt(25);
+    //z = abs(-21);
+    //z = ceil(x);
+    //z = floor(y);
+
+    cout << z;
+
+    return 0;
+}
