5a209dd03da6053d4084c3efb2dd7719fd558fcf
lucas <lucaszvunca033@gmail.com>
2026-07-12 12:48:48 +0100

    Create hello world!.cpp
    this is practice on the syntax of c++ and how to display hello world

---
+++ hello world!.cpp
@@ -0,0 +1,13 @@
+#include <iostream> //this is a basic header, input output
+
+int main(){
+    
+    std::cout << "hello world!";
+    /*
+    std means standard
+    cout means character out
+    << means output
+    */
+
+    return 0; // this is telling the program to close if there is no errors
+}
