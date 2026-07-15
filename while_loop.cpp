e90609a2ccf2bcddb0086ff0deba045ddaece88f
lucas <lucaszvunca033@gmail.com>
2026-07-15 22:15:09 +0100

    Update while_loop.cpp

--- while_loop.cpp
+++ while_loop.cpp
@@ -13,3 +13,19 @@ int main(){
 
     return 0;
 }
+------------------------------------------------------
+#include <iostream>
+
+int main(){
+
+    std::string name;
+
+    while(name.length() <= 5 || name.length() >= 15){
+        std::cout << "what is your name? ";
+        std::getline(std::cin, name);
+    }
+
+    std::cout << "hello " << name;
+
+    return 0;
+}
