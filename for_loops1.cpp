47300fbdd054b60cb7f32e49f78bbcc18a3487c8
lucas <lucaszvunca033@gmail.com>
2026-07-20 19:34:20 +0100

    Create for_loops1.cpp

---
+++ for_loops1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    
+    bool running = true;
+
+    if(running == true){
+        for(int i = 0; i <= 5; i++){
+            std::cout << "he is running " << "\n";
+        }
+        std::cout << "he has stopped running! ";
+    }
+
+    return 0;
+}
