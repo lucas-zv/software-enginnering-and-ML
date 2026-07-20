e5845b006c2da498f691f90f5bcec46c4fecc73e
lucas <lucaszvunca033@gmail.com>
2026-07-20 19:42:22 +0100

    Create for_loop2.cpp
    some for loop practice.

---
+++ for_loop2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    
+    for(int i = 0; i <= 5; i++){
+        std::cout << i << "\n";
+        for(int j = 0; j <= 5; j++){
+            std::cout << j << "\n";
+        }
+    }
+
+    return 0;
+}
